4de49bc775d14400ff84757d8d328916658d14a6
Abisena <abisena27@gmail.com>
2025-10-18 23:24:46 +0700

    fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,19 +6,18 @@ import { useAccount } from 'wagmi';
 import ConnectWallet from './atom/ConnectWallet';
 
 export default function Navbar() {
-// border-b border-black-300 shadow-md
     const [isOpen, setIsOpen] = useState(false);
     const [scrolled, setScrolled] = useState(false);
     const { address, isConnected } = useAccount();
 
     useEffect(() => {
         const handleScroll = () => {
-        const offset = window.scrollY;
-        if (offset > 50) {
-            setScrolled(true);
-        } else {
-            setScrolled(false);
-        }
+            const offset = window.scrollY;
+            if (offset > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
         };
 
         window.addEventListener("scroll", handleScroll);
@@ -26,34 +25,38 @@ export default function Navbar() {
     }, []);
 
     return (
-            <div className={`fixed w-full h-20 p-5 z-50 flex justify-center items-center text-black-primary transition-all duration-300 ${scrolled && "bg-white-secondary shadow-md"}`}>
+        <div className={`fixed w-full h-20 p-5 z-50 flex justify-center items-center text-black-primary transition-all duration-300 ${scrolled && "bg-white-secondary shadow-md"}`}>
             <div className="w-[90%] h-full flex justify-between items-center">
                 <div className="w-fit flex h-full gap-8 items-center font-semibold text-[15px] leading-[100%] tracking-[0]">
                     <div className="md:hidden text-black-primary">
                         <button className='cursor-pointer' onClick={() => setIsOpen(!isOpen)}>
                             {isOpen ? (
-                            <XMarkIcon className="h-6 w-6" />
+                                <XMarkIcon className="h-6 w-6" />
                             ) : (
-                            <Bars3Icon className="h-6 w-6" />
+                                <Bars3Icon className="h-6 w-6" />
                             )}
                         </button>
                     </div>
                     <img className="w-[100px]" src="/snap.svg" alt="snap logo" />
                     <div className="w-fit hidden md:flex h-full gap-8 items-center font-semibold text-[15px] leading-[100%] tracking-[0]">  
-                        <a className="hover:scale-110 transition-transform duration-300" href="">About</a>
-                        <a className="hover:scale-110 transition-transform duration-300" href="">Museum</a>
-                        <a className="hover:scale-110 transition-transform duration-300" href="">Collection</a>
-                        <a className="hover:scale-110 transition-transform duration-300" href="">Get Verify</a>
+                        <Link className="hover:scale-110 transition-transform duration-300" href="/about">About</Link>
+                        <Link className="hover:scale-110 transition-transform duration-300" href="/museum">Museum</Link>
+                        <Link className="hover:scale-110 transition-transform duration-300" href="/collection">Collection</Link>
+                        <Link className="hover:scale-110 transition-transform duration-300" href="/get-verify">Get Verify</Link>
                     </div>
                 </div>
                 <div className='flex gap-10 justify-center items-center'>
-                    <a className={`bg-blue-secondary ${isConnected ? "block" : "hidden"} p-2 rounded-3xl text-white-primary font-semibold border-1 border-solid border-black`} href="">Create a Snap</a>
+                    <Link 
+                      href="/brand/create"
+                      className={`bg-blue-secondary ${isConnected ? "block" : "hidden"} p-2 rounded-3xl text-white-primary font-semibold border-1 border-solid border-black`}
+                    >
+                      Create a Snap
+                    </Link>
                     <ConnectWallet />
                 </div>
             </div>
 
             {/* --- Menu Mobile Sidebar --- */}
-            {/* Background Overlay */}
             <div
                 className={`fixed inset-0 transition-opacity duration-300 ease-in-out z-30 ${
                 isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
@@ -61,30 +64,28 @@ export default function Navbar() {
                 onClick={() => setIsOpen(false)}
             ></div>
 
-            {/* Konten Menu */}
             <div
                 className={`fixed md:hidden top-0 left-0 h-screen w-3/4 max-w-sm shadow-2xl transform transition-transform duration-300 ease-in-out z-40 bg-white-secondary p-6 ${
                 isOpen ? 'translate-x-0' : '-translate-x-full'
                 }`}
             >
-                {/* Ganti 'bg-white-secondary' dengan warna Anda. Contoh: 'bg-gray-900 text-white' */}
                 <div className='flex py-4 gap-3 items-center'>
-                <div className="text-black-primary">
-                    <button className='cursor-pointer' onClick={() => setIsOpen(!isOpen)}>
-                        {isOpen ? (
-                        <XMarkIcon className="h-7 w-7" />
-                        ) : (
-                        <Bars3Icon className="h-6 w-6" />
-                        )}
-                    </button>
-                </div>
-                <img className="w-[100px]" src="/snap.svg" alt="snap logo" />
+                    <div className="text-black-primary">
+                        <button className='cursor-pointer' onClick={() => setIsOpen(!isOpen)}>
+                            {isOpen ? (
+                                <XMarkIcon className="h-7 w-7" />
+                            ) : (
+                                <Bars3Icon className="h-6 w-6" />
+                            )}
+                        </button>
+                    </div>
+                    <img className="w-[100px]" src="/snap.svg" alt="snap logo" />
                 </div>
                 <Link href="/about" className="w-fit block py-3 font-semibold text-2xl leading-[100%] hover:scale-110 transition-transform duration-300">About</Link>
-                <Link href="/services" className="w-fit block py-3 font-semibold text-2xl leading-[100%] hover:scale-110 transition-transform duration-300">Services</Link>
-                <Link href="/contact" className="w-fit block py-3 font-semibold text-2xl leading-[100%] hover:scale-110 transition-transform duration-300">Contact</Link>
+                <Link href="/museum" className="w-fit block py-3 font-semibold text-2xl leading-[100%] hover:scale-110 transition-transform duration-300">Museum</Link>
+                <Link href="/collection" className="w-fit block py-3 font-semibold text-2xl leading-[100%] hover:scale-110 transition-transform duration-300">Collection</Link>
+                <Link href="/get-verify" className="w-fit block py-3 font-semibold text-2xl leading-[100%] hover:scale-110 transition-transform duration-300">Get Verify</Link>
             </div>
         </div>
     )
 }
-
